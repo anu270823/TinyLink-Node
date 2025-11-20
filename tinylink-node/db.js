cf48447ba44a5d3ebc5c1c3b156abab905e0ebc0
anu270823 <anu270823@gmail.com>
2025-11-20 13:15:52 +0530

    db.js

--- tinylink-node/db.js
+++ tinylink-node/db.js
@@ -1,12 +1,14 @@
-// db.js
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // If using Heroku-style SSL:
-  // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected DB error', err);
 });
 
 module.exports = {
